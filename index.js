b32371a98bf8f6002a685fa0dd5408a590053a2e
Agarwal Neetu <agarwalneetu301@gmail.com>
2020-10-03 20:50:55 +0530

    added svg circle

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const startButton=document.querySelector('#start');
 const pauseButton=document.querySelector('#pause');
 const duration=document.querySelector('#duration');
 
+const circle=document.querySelector('circle');
+const perimeter=circle.getAttribute('r')*2*Math.PI;
+circle.setAttribute('stroke-dasharray',perimeter);
+
+let currentDuration;
+
 const timer= new Timer(duration,startButton,pauseButton,{
-    onStart(){
-        console.log("Timer started!!")
+    onStart(totalDuration){
+        console.log("Timer started!!");
+        currentDuration=totalDuration;
     },
-    onTick(){
-        console.log("Timer just ticked down!")
+    onTick(timeleft){
+        circle.setAttribute('stroke-dashoffset',
+        perimeter*timeleft/currentDuration-perimeter);
     },
     onComplete(){
         console.log("Timer completed !!!")
+        circle.setAttribute('stroke-dashoffset',-perimeter);
     }
 
 });
